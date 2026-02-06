09a5d945ab07bb0ef23789a83aa4b3ef04dccfab
mrysan <mrysann@gmail.com>
2026-02-05 16:07:42 -0800

    add create jwt instance method to user shema

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -12,17 +14,28 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide name"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      ,
       "Please provide a valid email",
     ],
-    unique: true, // creates unique index
+    unique: true, // creates unique index (cant have same address for 2 diff users)
   },
   password: {
     type: String,
     required: [true, "Please provide a password"],
     minlength: 6,
-    maxLength: 20,
   },
 });
+
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.createJWT = function () {
+  return jwt.sign(
+    { userId: this._id, name: this.name },
+    process.env.JWT_SECRET,
+    { expiresIn: "30d" },
+  );
+};
 
 module.exports = mongoose.model("User", UserSchema);
